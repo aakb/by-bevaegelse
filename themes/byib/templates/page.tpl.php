691640a6dc4c70363eea33d4ccbae0896ef52928
Aegir server <cableman0408@gmail.com>
2012-12-14 09:36:37 +0100

    Fixed permissons on folders

--- themes/byib/templates/page.tpl.php
+++ themes/byib/templates/page.tpl.php
@@ -71,21 +71,11 @@
 
 <div id="page">
 
-  <header class="grid-wrapper">
-  <div class="grid-inner">
-      <div class="grid-3-right">
-        <div class="header-inner">
-          <?php if ($logo): ?>
-            <div class="header-inner">
-              <!--<a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-                <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-              </a>-->
-            </div>
-          <?php endif; ?>
-        </div>
-      </div>
-    </div>
-  </header>  
+  <?php if ($logo): ?>
+    <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+      <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+    </a>
+  <?php endif; ?>  
   
   <?php if (!empty($page['branding'])): ?>
     <div class="grid-full branding">
