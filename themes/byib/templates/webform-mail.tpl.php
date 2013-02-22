b8951d24378330d7b362a44b993d97b218b61592
Rasmus Frey <rasmus@artz.dk>
2013-02-22 10:52:39 +0100

    Updated webform email template with html code

--- themes/byib/templates/webform-mail.tpl.php
+++ themes/byib/templates/webform-mail.tpl.php
@@ -19,21 +19,43 @@
  * when using the "default" e-mail template.
  */
 
-// Hardcoded that this is a HTML email.
+// Hardcoded that this is an HTML email.
 $email['html'] = 1;
 ?>
-<?php print ($email['html'] ? '<p>' : '') . t('Submitted on %date'). ($email['html'] ? '</p>' : ''); ?>
 
-<?php if ($user->uid): ?>
-<?php print ($email['html'] ? '<p>' : '') . t('Submitted by user: %username') . ($email['html'] ? '</p>' : ''); ?>
-<?php else: ?>
-<?php print ($email['html'] ? '<p>' : '') . t('Submitted by anonymous user: [%ip_address]') . ($email['html'] ? '</p>' : ''); ?>
-<?php endif; ?>
+<?php if $email['html'] : ?>
+<html>
+  <head>
+    <title></title>
+    <style type="text/css">
+    body {
+      font-family: "Helvitica Neue", Arial, Verdana, sans-serif;
+      font-size: 12px;
+    }
+    p {
+      margin-bottom: 15px;
+    }
+    </style>
+  </head>
+  <body>
+<?php endif ?>
 
-<?php print ($email['html'] ? '<p>' : '') . t('Submitted values are') . ':' . ($email['html'] ? '</p>' : ''); ?>
+    <?php print ($email['html'] ? '<p>' : '') . t('Submitted on %date'). ($email['html'] ? '</p>' : ''); ?>
 
-%email_values
+    <?php if ($user->uid): ?>
+    <?php print ($email['html'] ? '<p>' : '') . t('Submitted by user: %username') . ($email['html'] ? '</p>' : ''); ?>
+    <?php else: ?>
+    <?php print ($email['html'] ? '<p>' : '') . t('Submitted by anonymous user: [%ip_address]') . ($email['html'] ? '</p>' : ''); ?>
+    <?php endif; ?>
 
-<?php print ($email['html'] ? '<p>' : '') . t('The results of this submission may be viewed at:') . ($email['html'] ? '</p>' : '') ?>
+    <?php print ($email['html'] ? '<p>' : '') . t('Submitted values are') . ':' . ($email['html'] ? '</p>' : ''); ?>
 
-<?php print ($email['html'] ? '<p>' : ''); ?>%submission_url<?php print ($email['html'] ? '</p>' : ''); ?>
+    %email_values
+
+    <?php print ($email['html'] ? '<p>' : '') . t('The results of this submission may be viewed at:') . ($email['html'] ? '</p>' : '') ?>
+
+    <?php print ($email['html'] ? '<p>' : ''); ?>%submission_url<?php print ($email['html'] ? '</p>' : ''); ?>
+<?php if $email['html'] : ?>
+  </body>
+</html>
+<?php endif ?>
