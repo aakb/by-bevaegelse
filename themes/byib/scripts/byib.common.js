cd0ceac8e93025e72363a55419afc4b8a2a23b80
Jakob Rindom <rindom@gmail.com>
2016-01-20 13:50:49 +0100

    rindom: SUPPORT-430 Added submenu indicator in mobile menu

--- themes/byib/scripts/byib.common.js
+++ themes/byib/scripts/byib.common.js
@@ -1,4 +1,49 @@
+//https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+if (!String.prototype.repeat) {
+  String.prototype.repeat = function(count) {
+    'use strict';
+    if (this == null) {
+      throw new TypeError('can\'t convert ' + this + ' to object');
+    }
+    var str = '' + this;
+    count = +count;
+    if (count != count) {
+      count = 0;
+    }
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative');
+    }
+    if (count == Infinity) {
+      throw new RangeError('repeat count must be less than infinity');
+    }
+    count = Math.floor(count);
+    if (str.length == 0 || count == 0) {
+      return '';
+    }
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (;;) {
+      if ((count & 1) == 1) {
+        rpt += str;
+      }
+      count >>>= 1;
+      if (count == 0) {
+        break;
+      }
+      str += str;
+    }
+    return rpt;
+  }
+}
+
 (function ($) {
+
+
 
   /**
    * Add markup for main menu toggle.
@@ -59,7 +104,10 @@
       // Populate dropdown with menu items
       $(source).each(function() {
 
+
+
         var el = $(this);
+
         var children = el.find("li");
         var blockTitle = el.find(".block-title > a");
 
@@ -71,9 +119,11 @@
         }
 
         children.find("a").each(function() {
+          var sub = "-".repeat($(this).parents('li').length);
+
           $("<option />", {
             "value" : $(this).attr("href"),
-            "text": " - " + $(this).text()
+            "text": sub + $(this).text()
           }).appendTo("select:last");
         });
 
